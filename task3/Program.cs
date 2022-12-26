5e34347374d04c4924ef68e9ebcd9a85f5415aa6
Oleg <olegashepelev@gmail.com>
2022-12-26 20:12:16 +0600

    Complete third task

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Console.Write("Введите целое число: ");
+int number = int.Parse(Console.ReadLine() ?? "");
+
+int count = 1;
+
+Console.Write($"{number} -> ");
+while (count <= number)
+{
+    Console.Write($"{Math.Pow(count, 3)}, ");
+    count++;
+}
